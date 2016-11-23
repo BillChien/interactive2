5059bd66084f4e6eafd5fec6497a6af48c9d7087
Bill Chien <BillChien@Bills-MacBook-Pro.local>
2016-11-23 11:54:22 -0800

    update

--- project3/assets/script.js
+++ project3/assets/script.js
@@ -2,15 +2,12 @@ $('document').ready(function(){
 
   $('.layer_covers').mouseenter(function() {
     $(this).toggleClass("layer_covers_expand");
-    $('.layer_video1').toggleClass("slideout")
-      .one('transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd',
-      function (e) {
-       '.layer_video1'.removeClass('reveal');
-   });
+    $('.rs').toggleClass('showup');
   });
 
   $('.layer_covers').mouseleave(function() {
     $(this).removeClass("layer_covers_expand")
+    $('.rs').removeClass('showup');
   });
 
   $('.song1').mouseenter(function() {
@@ -83,12 +80,42 @@ $('document').ready(function(){
     $(this).removeClass("covers_expand")
   });
 
-
+  $('.rs').click(function() {
+    $(this).removeClass('showup');
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').removeClass('showup');
+  });
 
   $('.song1').click(function() {
     $(this).removeClass("covers_expand");
     $('.layer_covers').removeClass("layer_covers_expand");
-    $('.layer_video1').addClass("reveal");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video1').addClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
   });
+
+  $('.song2').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video2').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+  });
+
+  $('.song3').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video3').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+  });
+
+
 
 });
