0fc68167fb5e0b2df1f9680be074a65bea98be48
Bill Chien <BillChien@Bills-MacBook-Pro.local>
2016-11-26 09:26:40 -0800

    update

--- project3/assets/script.js
+++ project3/assets/script.js
@@ -84,6 +84,7 @@ $('document').ready(function(){
     $(this).removeClass('showup');
     $('.layer_covers').removeClass("layer_covers_expand");
     $('.layer_video').removeClass('showup');
+    $('.logo').removeClass('fadeout');
   });
 
   $('.song1').click(function() {
@@ -94,6 +95,14 @@ $('document').ready(function(){
     $('.video1').addClass('reveal');
     $('.video2').removeClass('reveal');
     $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
   });
 
   $('.song2').click(function() {
@@ -104,6 +113,14 @@ $('document').ready(function(){
     $('.video2').addClass('reveal');
     $('.video1').removeClass('reveal');
     $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
   });
 
   $('.song3').click(function() {
@@ -114,6 +131,140 @@ $('document').ready(function(){
     $('.video3').addClass('reveal');
     $('.video1').removeClass('reveal');
     $('.video2').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song4').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video4').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song5').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video5').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song6').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video6').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song7').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video7').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song8').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video8').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song9').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video9').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video10').removeClass('reveal');
+    $('.logo').addClass('fadeout');
+  });
+
+  $('.song10').click(function() {
+    $(this).removeClass("covers_expand");
+    $('.layer_covers').removeClass("layer_covers_expand");
+    $('.layer_video').addClass('showup');
+    $('.rs').removeClass('showup');
+    $('.video10').addClass('reveal');
+    $('.video1').removeClass('reveal');
+    $('.video2').removeClass('reveal');
+    $('.video3').removeClass('reveal');
+    $('.video4').removeClass('reveal');
+    $('.video5').removeClass('reveal');
+    $('.video6').removeClass('reveal');
+    $('.video7').removeClass('reveal');
+    $('.video8').removeClass('reveal');
+    $('.video9').removeClass('reveal');
+    $('.logo').addClass('fadeout');
   });
 
 
